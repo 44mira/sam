33c876bf5c43c16a0ea9ec00b30f3c5ab57f6f43
44mira <legolaslada@gmail.com>
2025-12-31 01:44:03 +0800

    feat!: redo scope handling in struct
    breaks current implementation, todo: fix

--- src/context.rs
+++ src/context.rs
@@ -1,21 +1,20 @@
 #![allow(dead_code)]
 
 use crate::value::Value;
-use std::collections::HashMap;
+use std::collections::{HashMap, hash_map::Entry};
+
+type SymbolTable = HashMap<String, Value>;
 
 #[derive(Debug)]
 pub struct Context {
-  pub env: HashMap<String, Value>,
-  pub scope_env: Option<HashMap<String, Value>>,
-
-  pub call_stack: Vec<String>,
+  // pub env: HashMap<String, Value>,
+  // pub scope_env: Option<HashMap<String, Value>>,
+  pub call_stack: Vec<SymbolTable>,
 }
 
 impl Context {
   pub fn new() -> Self {
     return Context {
-      env: HashMap::new(),
-      scope_env: None,
       call_stack: Vec::new(),
     };
   }
@@ -25,14 +24,23 @@ impl Context {
     return self.call_stack.len();
   }
 
-  pub fn init_scope(&mut self) {
-    // only initialize when scope_env is None
-    if !self.scope_env.is_none() {
-      return;
+  pub fn search_in_stack(
+    &mut self,
+    varname: &String,
+  ) -> Option<Entry<String, Value>> {
+    // find the first entry from the top of the stack that matches the variable
+    // name (lexical scoping)
+
+    let reverse_iter = self.call_stack.iter_mut().rev();
+
+    for table in reverse_iter {
+      if !table.contains_key(varname) {
+        continue;
+      }
+
+      return Some(table.entry(varname.to_owned()));
     }
 
-    let scope_env = HashMap::new();
-
-    self.scope_env = Some(scope_env);
+    return None;
   }
 }
